35373ebd4f04cb7a56b876112a5f57dec3a5850d
fulopzsolt1999 <fulopzsolt1999@gmail.com>
2024-07-21 11:59:01 +0200

    +

--- Scripts/activate.bat
+++ Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\fulop\Desktop\Django-Gym
+set VIRTUAL_ENV=C:\Users\fulop\OneDrive\Asztali gép\Django-Gym
 
 if not defined PROMPT set PROMPT=$P$G
 
