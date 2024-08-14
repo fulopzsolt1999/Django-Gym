0e8464f83a038a7a42f9a079667ff16618d41a87
fulopzsolt1999 <fulopzsolt1999@gmail.com>
2024-08-14 17:36:34 +0200

    + User signup is working (with password hashing)

--- FlexHub/SweatCircus/admin.py
+++ FlexHub/SweatCircus/admin.py
@@ -4,3 +4,5 @@ from . import models
 admin.site.register(models.Gym)
 admin.site.register(models.GymAddress)
 admin.site.register(models.City)
+admin.site.register(models.BodyGoals)
+admin.site.register(models.User)
