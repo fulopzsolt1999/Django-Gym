9fddc3d4ecdc7b9f5893eb86d0ff07d5df1fe64d
fulopzsolt1999 <fulopzsolt1999@gmail.com>
2024-08-25 16:51:01 +0200

    Big changes + Login, register system fully changed + Email sender function (password reset) + New pictures + Navbar, main page, about us page corrected + Styling almost done Needed - Handle phone screens (styling) - Login, register page restyling

--- FlexHub/SweatCircus/urls.py
+++ FlexHub/SweatCircus/urls.py
@@ -1,12 +1,9 @@
-from django.urls import path
-from .views import MainPage, GymSearch, PremiumPump, Signup, Login, AboutUs, ForgotPassword
+from django.urls import include, path
+from .views import MainPage, GymSearch, PremiumPump, AboutUs
 
 urlpatterns = [
     path('', MainPage, name="FlexHub"),
     path('gym-search/', GymSearch, name="GymSearch"),
     path('premium-pump/', PremiumPump, name="PremiumPump"),
     path('about-us/', AboutUs, name="AboutUs"),
-    path('signup/', Signup, name="Signup"),
-    path('login/', Login, name="Login"),
-    path('login/forgot-password', ForgotPassword, name="ForgotPassword")
 ]
--- FlexHub/SweatCircus/views.py
+++ FlexHub/SweatCircus/views.py
@@ -1,47 +1,16 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.views import LoginView
 from .models import City, GymAddress, Gym, BodyGoals, User
 import bcrypt
 
 def MainPage(request):
-   return render(request, "home.html")
+   return render(request, "base.html")
 
 def GymSearch(request):
    allGymData = Gym.objects.all()
    return render(request, "budgetBiceps.html", {"allGymData": allGymData})
 
-def PremiumPump(request):
-   return render(request, "premiumPump.html")
-
-def Signup(request):
-   bodyGoals = BodyGoals.objects.all()
-
-   if request.method == "POST":
-      email = request.POST.get("email")
-      hashedPW = bcrypt.hashpw(request.POST.get("password").encode("utf-8"), bcrypt.gensalt())
-      name = f"{request.POST.get("last-name")} {request.POST.get('first-name')}"
-      weight = request.POST.get("weight")
-      height = request.POST.get("height")
-      body_goals = request.POST.get("goal")
-
-      for goal in bodyGoals:
-         if goal.name == body_goals:
-            body_goals = goal
-
-      if all([email, hashedPW, name, weight, height, body_goals]):
-         User(email=email, password=hashedPW, name=name, weight=weight, height=height, body_goals=body_goals).save()
-
-      return redirect("Login")
-   
-   else:
-      return render(request, "signup.html", {"bodyGoals": bodyGoals})
-
-def Login(request):
-   return render(request, "login.html")
-
-def ForgotPassword(request):
-   return render(request, "forgotPassword.html")
-
 def AboutUs(request):
    return render(request, "aboutUs.html")
+
+def PremiumPump(request):
+   return render(request, "premiumPump.html")
