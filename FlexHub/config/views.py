49111eae2b2856d249f773ae845b463b13b8313d
Fülöp Zsolt <65891242+fulopzsolt1999@users.noreply.github.com>
2024-08-30 11:22:18 +0200

    Started working on register page, Styling fix on about us page

--- FlexHub/config/views.py
+++ FlexHub/config/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django import forms
 from django.shortcuts import render, redirect
+from pyexpat.errors import messages
 
 class RegistrationForm(UserCreationForm):
    email = forms.EmailField(required=True)
@@ -15,6 +16,18 @@ def registration_view(request):
    if request.method == "POST":
       form = RegistrationForm(request.POST)
       if form.is_valid():
+         username = form.cleaned_data.get("username")
+         email = form.cleaned_data.get("email")
+         # Check if username already exists
+         if User.objects.filter(username=username).exists():
+            messages.error(request, "Username is already taken.")
+            return redirect('register')
+
+         # Check if email already exists
+         if User.objects.filter(email=email).exists():
+            messages.error(request, "Email is already registered.")
+            return redirect('register')
+         
          form.save()
          return render(request, "premiumPump.html")
    else:
