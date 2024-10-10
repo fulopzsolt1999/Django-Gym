a1793b541923458a64218a84196da773745b9058
fulopzsolt1999 <fulopzsolt1999@gmail.com>
2024-10-10 17:48:21 +0200

    + Serializer added for send json data to javascript + Collect json gym data with fetch api + Search field added (working) - Correct styling

--- FlexHub/frontend/static/js/budgetBiceps.js
+++ FlexHub/frontend/static/js/budgetBiceps.js
@@ -1,67 +1,55 @@
-document.addEventListener("DOMContentLoaded", () => {
-   const selectedCity = document.querySelector("#city-select");
-   const selectedPrice = document.querySelector("#price-select");
-   const cityOptionText = document.querySelector("#city-option-text");
-   const priceOptionText = document.querySelector("#price-option-text");
-   const gymDataContainer = document.querySelectorAll(".gym-data");
-   const gymsAddresses = document.querySelectorAll(".gym-adress");
-   const gymsPrices = document.querySelectorAll(".gym-price");
+async function fetchAllData() {
+   const response = await fetch("http://127.0.0.1:8000/getSerializedGym&CityData");
+   const result = await response.json();
+   return result.map(gym => ({
+      id: gym.id,
+      name: gym.name,
+      address: gym.address.city_name.name + ", " + gym.address.street + " " + gym.address.street_number,
+      price_group: gym.price_group,
+      url: gym.url            
+   }));
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+   const allGymData = await fetchAllData();
+   const searchField = document.querySelector("#text-search");
+   const priceFilter = document.querySelector("#price-filter");
+   const allGymDataIds = allGymData.map(gym => gym.id);
    
-   document.querySelector("#filter-btn").addEventListener("click", () => {
-      const gymData = convertGymDataToDict();
-      if (selectedCity.value !== cityOptionText.textContent && selectedPrice.value !== priceOptionText.textContent) {
-         const filteredDataByCity = filteredDataByCity(gymData);
-         showFilteredData(filteredDataByPrice(filteredDataByCity))
-      } else if (selectedCity.value !== cityOptionText.textContent) {
-         showFilteredData(filteredDataByCity(gymData));
+   searchField.addEventListener("input", () => {
+      const searchTerm = searchField.value.toLowerCase();
+      let showGymIds = [];
+      if (searchTerm === "") {
+         showFilteredData(showGymIds=allGymDataIds, allGymDataIds);
       } else {
-         showFilteredData(filteredDataByPrice(gymData));
-      }
-   });
-   
-   function convertGymDataToDict() {
-      const gymData = [];
-      for (let i = 0; i < gymsAddresses.length; i++) {
-         gymData.push(
-            {
-               id: gymsAddresses[i].parentElement.parentElement.parentElement.id,
-               address: gymsAddresses[i].textContent.split(" ")[0],
-               price: gymsPrices[i].textContent
+         showGymIds = allGymData.map(gym => {
+            if (gym.name.toLowerCase().includes(searchTerm) || gym.address.split(",")[0].toLowerCase().includes(searchTerm)) {
+               return gym.id;
             }
-         )
+         }).filter(id => id !== undefined);      
       }
-      return gymData;
-   }
+      showFilteredData(showGymIds, allGymDataIds);
+   });
+});
 
-   function filteredDataByCity(gymData) {
-      const filteredGymDataByCity = [];
-      gymData.forEach(gym => {
-         if (gym.address === selectedCity.value) {
-            filteredGymDataByCity.push(gym);
-         }         
-      });
-      return filteredGymDataByCity;
-   }
-
-   function filteredDataByPrice(gymData) {
-      const filteredGymDataByPrice = [];
-      gymData.forEach(gym => {
-         if (gym.price === selectedPrice.value) {
-            filteredGymDataByPrice.push(gym);
-         }         
-      });
-      return filteredGymDataByPrice;
-   }
-
-   function showFilteredData(filteredData) {
-      for (let i = 0; i < filteredData.length; i++) {
-         for (let j = 0; j < gymsAddresses.length; j++) {
-            if (filteredData[i].id !== gymsAddresses[j].parentElement.parentElement.parentElement.id && filteredData[i].address !== gymsAddresses[j].textContent.split(" ")[0]) {
-               gymsAddresses[j].parentElement.parentElement.parentElement.classList.add("d-none");
-            } else {
-               gymsAddresses[j].parentElement.parentElement.parentElement.classList.remove("d-none");
+function showFilteredData(showGymIds, allGymDataIds) {
+   if (showGymIds.length > 0) {
+      for (let gymId of allGymDataIds) {
+         if (showGymIds.includes(gymId)) {
+            if (document.querySelector(`#id-${gymId}`).classList.contains("d-none")) {
+               document.querySelector(`#id-${gymId}`).classList.remove("d-none");
+            }
+         } else {
+            if (!document.querySelector(`#id-${gymId}`).classList.contains("d-none")) {
+               document.querySelector(`#id-${gymId}`).classList.add("d-none");
             }
          }
       }
+   } else {
+      for (let gymId of allGymDataIds) {
+         if (!document.querySelector(`#id-${gymId}`).classList.contains("d-none")) {
+            document.querySelector(`#id-${gymId}`).classList.add("d-none");
+         }
+      }
    }
-});
+}
