0e5c02103f87172b95aed5be032cc04a78f10cc7
fulopzsolt1999 <fulopzsolt1999@gmail.com>
2024-10-13 15:45:28 +0200

    + Optimized gym search page (faster js run)

--- FlexHub/frontend/static/js/budgetBiceps.js
+++ FlexHub/frontend/static/js/budgetBiceps.js
@@ -13,43 +13,89 @@ async function fetchAllData() {
 document.addEventListener("DOMContentLoaded", async () => {
    const allGymData = await fetchAllData();
    const searchField = document.querySelector("#text-search");
-   const priceFilter = document.querySelector("#price-filter");
+   const priceFilterSelect = document.querySelector("#price-select");
    const allGymDataIds = allGymData.map(gym => gym.id);
    
    searchField.addEventListener("input", () => {
       const searchTerm = searchField.value.toLowerCase();
       let showGymIds = [];
-      if (searchTerm === "") {
-         showFilteredData(showGymIds=allGymDataIds, allGymDataIds);
-      } else {
+      if (searchTerm !== "") {
          showGymIds = allGymData.map(gym => {
             if (gym.name.toLowerCase().includes(searchTerm) || gym.address.split(",")[0].toLowerCase().includes(searchTerm)) {
                return gym.id;
             }
-         }).filter(id => id !== undefined);      
+         }).filter(id => id !== undefined);
       }
-      showFilteredData(showGymIds, allGymDataIds);
+      showFilteredData(showGymIds, allGymDataIds, allGymData);
    });
+   showFilteredData(showGymIds=allGymDataIds, allGymDataIds, allGymData);
 });
 
-function showFilteredData(showGymIds, allGymDataIds) {
+function showFilteredData(showGymIds, allGymDataIds, allGymData) {
+   const gymsContainer = document.querySelector("#gyms-container");
+   gymsContainer.innerHTML = "";
+ 
    if (showGymIds.length > 0) {
-      for (let gymId of allGymDataIds) {
-         if (showGymIds.includes(gymId)) {
-            if (document.querySelector(`#id-${gymId}`).classList.contains("d-none")) {
-               document.querySelector(`#id-${gymId}`).classList.remove("d-none");
-            }
-         } else {
-            if (!document.querySelector(`#id-${gymId}`).classList.contains("d-none")) {
-               document.querySelector(`#id-${gymId}`).classList.add("d-none");
-            }
-         }
-      }
+     const filteredGyms = allGymData.filter(gym => showGymIds.includes(gym.id));
+     gymsContainer.append(...filteredGyms.map(createGymsContainerContent));
    } else {
-      for (let gymId of allGymDataIds) {
-         if (!document.querySelector(`#id-${gymId}`).classList.contains("d-none")) {
-            document.querySelector(`#id-${gymId}`).classList.add("d-none");
-         }
-      }
+     gymsContainer.append(...allGymData.map(createGymsContainerContent));
    }
-}
+ }
+
+function createGymsContainerContent(gym) {
+   const gymContainerDiv = document.createElement("div");
+   const gymDataDiv = document.createElement("div");
+   const h3Tag = document.createElement("h3");
+ 
+   const gymDataConfig = {
+     addressP: { icon: "fa-solid fa-location-dot", spanText: gym.address },
+     linkP: { icon: "fa-solid fa-link", aTagHref: gym.url, aTagTarget: "_blank", aTagText: "Weboldal link / Facebook link" },
+     priceP: { icon: "fa-solid fa-link", spanText: "Árkategória:", gymPriceSpanClass: "gym-price", gymPriceSpanText: gym.price_group },
+   };
+ 
+   Object.keys(gymDataConfig).forEach((key) => {
+     const config = gymDataConfig[key];
+     const pTag = document.createElement("p");
+     const spanTag = document.createElement("span");
+     const iTag = document.createElement("i");
+ 
+     iTag.classList.add(...config.icon.split(" "));
+ 
+     spanTag.textContent = config.spanText;
+ 
+     if (config.spanClass) {
+       spanTag.classList.add(config.spanClass);
+     } else if (config.aTagHref) {
+       const aTag = document.createElement("a");
+       aTag.href = config.aTagHref;
+       aTag.target = config.aTagTarget;
+       aTag.textContent = config.aTagText;
+       pTag.appendChild(iTag);
+       pTag.appendChild(aTag);
+     } else if (config.gymPriceSpanClass) {
+       const gymPriceSpan = document.createElement("span");
+       gymPriceSpan.classList.add(config.gymPriceSpanClass);
+       gymPriceSpan.textContent = config.gymPriceSpanText;
+       pTag.appendChild(iTag);
+       pTag.appendChild(gymPriceSpan);
+     } else {
+       pTag.appendChild(iTag);
+       pTag.appendChild(spanTag);
+     }
+ 
+     pTag.classList.add("m-0");
+     gymDataDiv.appendChild(pTag);
+   });
+ 
+   h3Tag.classList.add("m-1", "mb-2", "ms-3");
+   h3Tag.textContent = gym.name;
+ 
+   gymDataDiv.classList.add("m-auto", "mt-4", "ps-3");
+ 
+   gymContainerDiv.classList.add("col-4", "p-2", "gym-data", `id-${gym.id}`);
+   gymContainerDiv.appendChild(h3Tag);
+   gymContainerDiv.appendChild(gymDataDiv);
+ 
+   return gymContainerDiv;
+ }
