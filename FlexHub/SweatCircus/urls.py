763998578f641e0bf1b52eec72e2ea352cd8a62e
fulopzsolt1999 <fulopzsolt1999@gmail.com>
2024-11-09 17:43:19 +0100

    Some progress with the basic concepts of the workout plan site

--- FlexHub/SweatCircus/urls.py
+++ FlexHub/SweatCircus/urls.py
@@ -1,10 +1,12 @@
 from django.urls import include, path
-from .views import MainPage, GymSearch, PremiumPump, AboutUs, getSerializedGymAndCityData
+from .views import MainPage, GymSearch, PremiumPump, WorkoutPlanUpdate, AboutUs, getSerializedGymAndCityData, getSerializedWorkoutPlansData
 
 urlpatterns = [
     path('', MainPage, name="FlexHub"),
     path('getSerializedGym&CityData/', getSerializedGymAndCityData, name="getSerializedGymAndCityData"),
+    path('getSerializedWorkoutPlansData/', getSerializedWorkoutPlansData, name="getSerializedWorkoutPlansData"),
     path('gym-search/', GymSearch, name="GymSearch"),
     path('premium-pump/', PremiumPump, name="PremiumPump"),
+    path('premium-pump/workout-plan-update/', WorkoutPlanUpdate, name="WorkoutPlanUpdate"),
     path('about-us/', AboutUs, name="AboutUs"),
 ]
