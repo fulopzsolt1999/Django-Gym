db7866f2efc0be1649879db0948bd80275ad5d21
fulopzsolt1999 <fulopzsolt1999@gmail.com>
2024-12-05 19:18:41 +0100

    Login, Register styling is finished for PC - Mobile styling needed!!

--- FlexHub/config/views.py
+++ FlexHub/config/views.py
@@ -1,19 +1,46 @@
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.views import LoginView
 from django.contrib.auth import login
 from django import forms
 from django.shortcuts import render, redirect
 from pyexpat.errors import messages
 
 class RegistrationForm(UserCreationForm):
-   username = forms.CharField(required=True)
-   email = forms.EmailField(required=True)
-   first_name = forms.CharField(required=True)
-   last_name = forms.CharField(required=True)
+   username = forms.CharField(
+      required = True, 
+      widget = forms.TextInput(attrs={'class': 'form-control'}))
+   email = forms.EmailField(
+      required = True,
+       widget=forms.EmailInput(attrs={'class': 'form-control'}))
+   first_name = forms.CharField(
+      required = True, 
+      widget = forms.TextInput(attrs={'class': 'form-control'}))
+   last_name = forms.CharField(
+      required=True, 
+      widget = forms.TextInput(attrs={'class': 'form-control'}))
+   password1 = forms.CharField(
+      required = True, 
+      widget = forms.PasswordInput(attrs={'class': 'form-control'}))
+   password2 = forms.CharField(
+      required = True, 
+      widget = forms.PasswordInput(attrs={'class': 'form-control'}))
 
    class Meta:
       model = User
       fields = ["username", "email", "first_name", "last_name", "password1", "password2"]
+
+class LoginForm(AuthenticationForm):
+   class Meta:
+      model = User
+      fields = ['username', 'password']
+      widgets = { 
+         'username': forms.TextInput(attrs={'class': 'form-control'}),
+         'password': forms.PasswordInput(attrs={'class': 'form-control'}) }
+
+class CustomLoginView(LoginView):
+   form_class = LoginForm
+   template_name = 'registration/login.html'
 
 def registration_view(request):
    if request.method == "POST":
--- FlexHub/frontend/static/js/register.js
+++ FlexHub/frontend/static/js/register.js
@@ -1,14 +1,3 @@
 document.addEventListener("DOMContentLoaded", () => {
-   const form = document.querySelector("form");
-   const formDivElements = form.querySelectorAll("div");
-   const inputElements = form.querySelectorAll("div input");
-   const pw1HelpTextDiv = document.querySelector("#id-password1-helptext")
-   const pw2HelpTextDiv = document.querySelector("#id-password2-helptext")
-   formDivElements.forEach(div => {
-      if (!div.classList.contains("helptext")) {
-         console.log(div);
-         
-      }
-      div.setAttribute("class", "form-group");
-   });
+   
 });
